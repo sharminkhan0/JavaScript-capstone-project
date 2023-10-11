f3b2da163ccf0ed191dc77aaf5106ba7e1ec5a2b
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-12 03:05:01 +0600

    Add style to navbar, ul,li inside the header

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/', // Specify the output directory for images
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
