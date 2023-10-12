2062081c7db0b60af36210413e61e34b95f444d2
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-13 01:16:03 +0600

    fix eslint error

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,14 +1,9 @@
 const getMovies = async () => {
+  const response = await fetch('https://api.tvmaze.com/shows');
 
-    const response = await fetch('https://api.tvmaze.com/shows');
-  
-    const shows = await response.json();
-  
-    return shows;
-  
-  };
-  
-  
-  
-  
-  export default getMovies;
+  const shows = await response.json();
+
+  return shows;
+};
+
+export default getMovies;
