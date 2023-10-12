05525f9b7242fc34534ff10c78cdc9b0f6e2cce7
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-13 02:13:59 +0600

    add api link to popup window

--- src/index.js
+++ src/index.js
@@ -1,59 +1,77 @@
 import './styles/index.css';
 import './images/MovieLogo-1.png';
-
+import './images/close_icon.png';
 import displayMovies from './modules/displayMovies.js';
 import getMovies from './modules/api.js';
 
 const displayAllMovies = async () => {
-  const display = await getMovies();
-
-  displayMovies(display);
-};
-
-window.addEventListener('load', displayAllMovies);
-const parentElem = document.getElementById('liContainer');
-parentElem.addEventListener('click', (event) => {
-  if (event.target.matches('.btn')) {
-    const newDiv = document.createElement('div');
-    newDiv.className = "mainContainer";
-    newDiv.innerHTML = `
-    <div class="mainContainer1">
-    <div class="closeBtd flex">
-        <img src="./modules/close_icon.png" alt="main image">
-    </div>
-    <div class="movieDetails flex">
-        <div class="imgDiv">
-           <img src="./modules/rsz_popup_mobile_thumbnail.png" alt="main image">
-        </div>
-        <h2>Movie Name</h2>
-        <div class="movieInfo flex">
-            <div class="otherInfo flex">
-                <span>Language</span>
-                <span>Genres</span>
-            </div>
-            <div class="otherInfo flex">
-                <span>Episode Length</span>
-                <span>Genres</span>
-            </div>
-        </div>
-        <div class="comments flex">
-            <h3>Comments</h3>
-            <div class="commentsDisplay flex">
-                <span>Nahid : Love this movie</span>
-            </div>
-        </div>
-        <div class="addComment flex">
-            <h3>Add a comment</h3>
-            <div class="commentField flex">
-                <input type="text" class="name" placeholder="Your name">
-                <textarea name="" id="" cols="30" rows="5" placeholder="Your insights"></textarea>
-                <button>Comment</button>
-            </div>
-        </div>
-    </div>
-</div>
-    `
-    document.body.appendChild(newDiv);
-  }
-});
-// console.log(parentElem);
+    const display = await getMovies();
+    displayMovies(display);
+  };
+  
+  window.addEventListener('load', displayAllMovies);
+  
+  const shows = await getMovies();
+  const arr = Array.from(shows);
+  
+  const parentElem = document.getElementById('liContainer');
+  parentElem.addEventListener('click', (event) => {
+    const matcher = event.target.matches('.btn');
+    const eventElem = event.target;
+    const eventId = eventElem.id - 1;
+  
+    if (matcher) {
+      const newDiv = document.createElement('div');
+      newDiv.className = 'mainContainer';
+      newDiv.innerHTML = `
+      <div class="mainContainer1">
+      <div class="closeBtd flex">
+          <img id="closeImg" src="./images/close_icon.png" alt="main image">
+      </div>
+      <div class="movieDetails flex">
+          <div class="imgDiv">
+             <img src="${arr[eventId].image.medium}">
+          </div>
+          <h2>${arr[eventId].name}</h2>
+          <div class="movieInfo flex">
+              <div class="otherInfo flex">
+                  <span>Language: ${arr[eventId].language}</span>
+                  <span>Genres : ${arr[eventId].genres}</span>
+              </div>
+              <div class="otherInfo flex">
+                  <span>Episode Length : ${arr[eventId].averageRuntim}</span>
+                  <span>Rating : ${arr[eventId].rating.average}</span>
+              </div>
+          </div>
+          <div class="comments flex">
+              <h3>Comments</h3>
+              <div class="commentsDisplay flex">
+                  <span>Nahid : Love this movie</span>
+              </div>
+          </div>
+          <div class="addComment flex">
+              <h3>Add a comment</h3>
+              <div class="commentField flex">
+                  <input type="text" class="name" placeholder="Your name">
+                  <textarea name="" id="" cols="30" rows="5" placeholder="Your insights"></textarea>
+                  <button>Comment</button>
+              </div>
+          </div>
+      </div>
+  </div>
+      `;
+      document.body.appendChild(newDiv);
+    }
+  });
+  
+  const bodyHtml = document.getElementById('liContainer');
+  const parent = bodyHtml.parentElement.parentElement;
+  
+  parent.addEventListener('click', (event) => {
+    if (event.target.matches('#closeImg')) {
+      const lastCh = parent.lastChild;
+      if (lastCh) {
+        document.body.removeChild(lastCh);
+      }
+    }
+  });
