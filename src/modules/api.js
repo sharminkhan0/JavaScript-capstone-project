ce5b1abc64774300392deb7125603a31a8fc09d8
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-13 01:11:55 +0600

    Create modules directory, api.js and displayMovies.js files

---
+++ src/modules/api.js
@@ -0,0 +1,14 @@
+const getMovies = async () => {
+
+    const response = await fetch('https://api.tvmaze.com/shows');
+  
+    const shows = await response.json();
+  
+    return shows;
+  
+  };
+  
+  
+  
+  
+  export default getMovies;
