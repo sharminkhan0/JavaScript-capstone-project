f263f82793895236ff97cda33b7c2d717074cfc2
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-13 01:26:54 +0600

    Add section for displaying all cards

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './styles/index.css';
 import './images/MovieLogo-1.png';
+
+import displayMovies from './modules/displayMovies.js';
+import getMovies from './modules/api.js';
+
+
+
+
+const displayAllMovies = async () => {
+
+  const display = await getMovies();
+
+  displayMovies(display);
+
+};
+
+
+
+
+window.addEventListener('load', displayAllMovies);
