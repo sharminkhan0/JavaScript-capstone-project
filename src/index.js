b35e0603a422da40557399b3f8bce73e6ff4f534
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-13 01:34:36 +0600

    fix eslint error

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ import './images/MovieLogo-1.png';
 import displayMovies from './modules/displayMovies.js';
 import getMovies from './modules/api.js';
 
-
-
-
 const displayAllMovies = async () => {
-
   const display = await getMovies();
 
   displayMovies(display);
-
 };
-
-
-
 
 window.addEventListener('load', displayAllMovies);
