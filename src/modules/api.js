b6a8f9a0d8516c3eedd795edf5aae95cf1f6d96e
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-10-14 02:46:57 +0600

    Update dist directory

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,5 +1,5 @@
 const getMovies = async () => {
-  const response = await fetch('https://api.tvmaze.com/shows');
+  const response = await fetch('https://api.tvmaze.com/shows/');
 
   const shows = await response.json();
 
